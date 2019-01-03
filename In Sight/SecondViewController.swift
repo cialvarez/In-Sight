c1279c8b395a558569039ec51247c85f6e49a50f
Christian Iñigo De Leon Alvarez <Christian.Alvarez@travelbook.ph>
2019-01-03 22:36:41 +0800

    Initial Commit

---
+++ In Sight/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  In Sight
+//
+//  Created by Christian Iñigo De Leon Alvarez on 03/01/2019.
+//  Copyright © 2019 Freelancer. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
