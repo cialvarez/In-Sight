3118ed6051d9813ff8638832ea40f19b683a1734
Christian Iñigo De Leon Alvarez <Christian.Alvarez@travelbook.ph>
2019-01-04 17:05:00 +0800

    Add skeletal files

---
+++ In Sight/Controllers/Common/Gallery/Views/ImageCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCell.swift
+//  In Sight
+//
+//  Created by Christian Iñigo De Leon Alvarez on 04/01/2019.
+//  Copyright © 2019 Freelancer. All rights reserved.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
